f1b61eab4b756c0e1dcb9b1d683a53b073366330
LukeYan <925102171@qq.com>
2022-03-22 00:12:41 +0800

    尝试添加pwa功能

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,10 +9,6 @@ module.exports = {
             lang: 'zh-CN'
         }
     },
-    head: [
-        ["link", { rel: 'icon', href: '/logo.svg' }],
-        ["meta", { name: 'viewport', content: "width=device-width,initial-scale=1,user-scalable=no" }],
-    ],
     theme: 'reco',
     themeConfig: {
         logo: '/logo.svg',
@@ -60,5 +56,22 @@ module.exports = {
     },
     markdown: {
         lineNumbers: true
-    }
+    },
+
+    head: [
+        ["link", { rel: 'icon', href: '/logo.svg' }],
+        ["meta", { name: 'viewport', content: "width=device-width,initial-scale=1,user-scalable=no" }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ],
+    plugins: ['@vuepress/pwa', {
+        ServiceWorker: true,
+        updatePopup: true
+    }]
 }
